20537535fe8696d237cdde3c68e59784dbc0d48d
arashfasihi <arashfasihi@yahoo.com>
2022-05-17 13:36:01 +0200

    Update file.cs

--- file.cs
+++ file.cs
@@ -1,6 +1,11 @@
-using System;
-
-void func()
+public StreamWriter AllocatedStreamWriter()
 {
-  Console.Writeline("I am a func. changed again");
+    FileStream fs = File.Create("everwhat.txt");
+    return new StreamWriter(fs);
+}
+
+public void ResourceLeakBad()
+{
+    StreamWriter stream = AllocateStreamWriter();
+    // FIXME: should close the StreamWriter by calling stream.Close() if stream is not null.
 }
